f2798b282bc2e08b5f238ee267d22a3b0a558850
Scott-fo <scottfo@live.com>
2023-09-22 18:46:23 +0100

    feat: finish 2022 day2 part 1

--- 2022/day1/main.cc
+++ 2022/day1/main.cc
@@ -1,44 +1,33 @@
 #include <algorithm>
-#include <cstdio>
-#include <cstdlib>
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <ostream>
+#include <iterator>
+#include <numeric>
 #include <string>
 #include <vector>
 
 int main() {
   std::ifstream fin("input.txt");
+  std::vector<int> sums{};
 
-  std::vector<int> sums = {};
   int sum = 0;
 
-  while (fin) {
-    std::string calorie;
-    getline(fin, calorie);
-
-    if (calorie.empty()) {
+  for (std::string line; std::getline(fin, line);) {
+    if (line.empty()) {
       sums.push_back(sum);
       sum = 0;
-
-      continue;
+    } else {
+      sum += std::stoi(line);
     }
-
-    sum += std::stoi(calorie);
   }
 
   sums.push_back(sum);
 
-  double max = *std::max_element(sums.begin(), sums.end());
+  const auto max = *std::max_element(sums.begin(), sums.end());
   std::cout << "Max Element: " << max << std::endl;
 
   std::sort(sums.begin(), sums.end(), std::greater<int>());
-
-  int top3 = 0;
-  for (int i = 0; i < 3; ++i) {
-    top3 += sums[i];
-  }
+  const auto top3 = std::accumulate(sums.begin(), sums.begin() + 3, 0);
 
   std::cout << "Sum of Top 3: " << top3 << std::endl;
 
--- 2022/day2/main.cc
+++ 2022/day2/main.cc
@@ -1,82 +1,73 @@
 #include <cassert>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
 
-enum moves_t { rock, paper, scissors };
-enum outcomes_t { win, lose, draw };
+enum class moves_t { rock, paper, scissors };
+enum class outcomes_t { win, lose, draw };
 
 moves_t get_move(char c) {
-  switch (c) {
-  case 'A':
-    return rock;
-  case 'X':
-    return rock;
-  case 'B':
-    return paper;
-  case 'Y':
-    return paper;
-  case 'C':
-    return scissors;
-  case 'Z':
-    return scissors;
-  default:
-    assert("Unreachable");
-    return rock;
+  static const std::unordered_map<char, moves_t> char_to_move = {
+      {'A', moves_t::rock},  {'X', moves_t::rock},     {'B', moves_t::paper},
+      {'Y', moves_t::paper}, {'C', moves_t::scissors}, {'Z', moves_t::scissors},
+  };
+
+  auto move = char_to_move.find(c);
+  if (move == char_to_move.end()) {
+    throw std::runtime_error("Unexepected character: " + std::string(1, c));
   }
+
+  return move->second;
 }
 
 int get_move_score(moves_t m) {
-  if (m == rock) {
-    return 1;
-  }
+  static const std::unordered_map<moves_t, int> move_to_score = {
+      {moves_t::rock, 1},
+      {moves_t::paper, 2},
+      {moves_t::scissors, 3},
+  };
 
-  if (m == paper) {
-    return 2;
-  }
+  return move_to_score.at(m);
+}
 
-  if (m == scissors) {
-    return 3;
-  }
+int get_outcome_score(outcomes_t o) {
+  static const std::unordered_map<outcomes_t, int> outcome_to_score = {
+      {outcomes_t::win, 6},
+      {outcomes_t::draw, 3},
+      {outcomes_t::lose, 0},
+  };
 
-  assert("Unreachable");
-  return 0;
+  return outcome_to_score.at(o);
 }
 
 outcomes_t get_outcome(moves_t mx, moves_t my) {
   if (mx == my) {
-    return draw;
+    return outcomes_t::draw;
   }
 
-  if ((mx == rock && my == scissors) || (mx == paper && my == rock) ||
-      (mx == scissors && my == paper)) {
-    return win;
+  if ((mx == moves_t::rock && my == moves_t::scissors) ||
+      (mx == moves_t::paper && my == moves_t::rock) ||
+      (mx == moves_t::scissors && my == moves_t::paper)) {
+    return outcomes_t::win;
   }
 
-  return lose;
-}
-
-int get_outcome_score(outcomes_t o) {
-  if (o == win) {
-    return 6;
-  }
-
-  if (o == lose) {
-    return 0;
-  }
-
-  return 3;
+  return outcomes_t::lose;
 }
 
 int main() {
-  std::string moves[] = {
-      "A Y",
-      "B X",
-      "C Z",
-  };
+  std::ifstream fin("input.txt");
+  if (!fin.is_open()) {
+    std::cerr << "Failed to open input.txt" << std::endl;
+    return 1;
+  }
 
   int total_score = 0;
-  for (auto move : moves) {
-    moves_t opp_move = get_move(move[0]);
-    moves_t your_move = get_move(move[2]);
+
+  for (std::string line; std::getline(fin, line);) {
+    moves_t opp_move = get_move(line[0]);
+    moves_t your_move = get_move(line[2]);
 
     total_score += get_move_score(your_move);
     total_score += get_outcome_score(get_outcome(your_move, opp_move));
